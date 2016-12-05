9016b73f57fc6bd8cfeaea0f01170f19a726e73a
Andrei Belokopytov <belokopytov87@gmail.com>
2016-12-05 11:32:06 +0300

    Add clients data and Client model.

---
+++ src/app/models/client.ts
@@ -0,0 +1,35 @@
+export class Client {
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  company: string;
+  jobTitle: string;
+  email: string;
+  phone: string;
+  city: string;
+  street: string;
+  zipCode: string;
+  country: string;
+
+  fromJSON(json: any) {
+    try {
+      this.firstName = json.general.firstName;
+      this.lastName = json.general.lastName;
+      this.avatar = json.general.avatar;
+      this.company = json.job.company;
+      this.jobTitle = json.job.title;
+      this.email = json.contact.email;
+      this.phone = json.contact.phone;
+      this.city = json.address.city;
+      this.street = json.address.street;
+      this.zipCode = json.address.zipCode;
+      this.country = json.address.country;
+    } catch (err) {
+      console.log(`Failed to create Client from JSON: ${err.message}.t`);
+    }
+  }
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
